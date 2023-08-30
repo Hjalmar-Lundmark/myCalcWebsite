a79ecdedb2d2369d0ce1581b7f2d521d6efd3939
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-08-30 22:48:39 +0200

    Fix: Navbar padding finally

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,4 +3,7 @@ const button = document.querySelector('.navbar__button');
 button.addEventListener('click', function(event) {
     const menu = document.querySelector('.navbar__collapse');
     menu.classList.toggle('navbar__collapse--show');
+
+    const navbar = document.querySelector('.navbar__inner');
+    navbar.classList.toggle('navbar__inner--open');
 });
